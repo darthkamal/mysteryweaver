a57fd4e37a21aad1de5c3fd4a4e4beefa9f514a9
Daniel Togun <danieltogun@gmail.com>
2026-05-14 01:39:29 +0200

    feat(engine-schema): add CharactersSchema with variant flag support

--- packages/engine-schema/src/index.ts
+++ packages/engine-schema/src/index.ts
@@ -1,2 +1,5 @@
 export { ManifestSchema } from './manifest.schema.js'
 export type { Manifest, Currency, Phase } from './manifest.schema.js'
+
+export { CharactersSchema } from './characters.schema.js'
+export type { Characters, Character, PublicProfile, PrivateProfile } from './characters.schema.js'
